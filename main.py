3c9afdd8e0178e9349d1f128905ba0a7e1ad7335
Luan Leite <luanleite202@gmail.com>
2024-11-07 11:13:00 -0300

    Fix str verification error

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 '''
 Esse codigo ainda será atualizado, está em teste.
 
-Mas já funcionando
+-- Atualizações previstas:
+.Mais verificações para desempenho;
+.Otimizar todo o codigo.
+
 '''
 import mysql.connector
-from zinfo import userInfoRequest, User, add
+from zinfo import userInfoRequest, User, add, userInfo, users
  # type: ignore
 import os
 
-os.system("cls")
 
-user = userInfoRequest()
 
-add(user)
+while True:
+    os.system("cls")
+    user = userInfoRequest()
+    parar = input("Deseja adicionar mais algum usuário?\n 's' para SIM ou 'n' para NÃO" )
+    if parar.lower() == "s":
+        print(users)
+        continue
+    else:
+        break
+
+
 
 
 
--- zinfo.py
+++ zinfo.py
@@ -3,16 +3,16 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
 # DADOS DO USUARIO
 class User:
-    def __init__(self, nome, idade, cargo, empresa, salario, meta, economiaMensal, poupanca):
+    def __init__(self, nome, idade, cargo, empresa, renda, meta, economiaMensal, poupanca):
         self.nome = nome
         self.idade = idade
         self.cargo = cargo
         self.empresa = empresa
-        self.salario = float(salario)
+        self.renda = float(renda)
         self.meta = float(meta)
         self.economiaMensal = float(economiaMensal)
         self.poupanca = float(poupanca)
@@ -23,15 +23,92 @@ class User:
             f"Idade: {self.idade},\n"
             f"Cargo: {self.cargo},\n"
             f"Empresa: {self.empresa},\n"
-            f"Salário: R${self.salario:.2f},\n"
+            f"Salário: R${self.renda:.2f},\n"
             f"Meta: R${self.meta:.2f},\n"
             f"Economia Mensal: R${self.economiaMensal:.2f},\n"
             f"Poupança: R${self.poupanca:.2f},\n"
             f"Meses Restantes: {self.mesesRestantes}")
 
+#Lista de usuarios
+users = []
 
+#Adicionando usuario em uma lista.
+def userInfo(users, user):
+    users.append(user)
+    print(f"{user.nome} listado.")
 
-# FUNÇÃO PARA ADICIONAR INFORMAÇÕES NO MySQL
+#Validar STR
+def validarSTR(string):
+    if all(i.isalpha() or i.isspace() for i in string): 
+        return True
+    else:
+        return False
+
+#Validar NUM
+def validarNUM(number):
+    if len(number) > 2:
+        try:
+            number = float(number)
+            return number > 0
+        except ValueError:
+            print("Digite o numero corretamente.")
+    else:
+        try:
+            number = int(number)
+            return number > 0
+        except ValueError:
+            print("Digite o numero corretamente.")
+    
+
+#solicitando dados do usuario.
+def userInfoRequest():
+    # nome
+    nome = input("Nome: \nR: ")  
+    while not validarSTR(nome):
+        print("Por favor, insira um NOME VALIDO.")
+        nome = input("Nome: \n")
+
+    #idade
+    idade = input("Idade: \nR: ")
+    while not validarNUM(idade):
+        print(" Por favor, insira uma idade válida.")
+        idade = input("Idade: \n")
+    
+    #empregado = True or False
+    empregado = input("Está trabalhando atualmente? 's' para SIM ou 'n' para NÃO\nR: ")
+
+    while not validarSTR(empregado):
+        print("Por favor, insira caracteres válidos.")
+        empregado = input("Está trabalhando atualmente? 's' para SIM ou 'n' para NÃO\nR: ")
+        if empregado.lower() == "s":  
+            cargo = input("Qual seu cargo?  \nR: ")
+            empresa = input("Empresa:\nR: ")
+        else:
+            cargo = ""
+            empresa = ""
+    
+
+    renda = input("Qual a sua renda?\nR: ")
+    meta = input("Quanto pretende juntar?\nR: ")
+    economiaMensal = input("Quanto costuma guardar por mês?\nR: ")
+    poupanca = input("Quanto você já possui? \nR: ")
+
+    try:
+        renda = float(renda)
+        meta = float(meta)
+        economiaMensal = float(economiaMensal)
+        poupanca = float(poupanca)
+        user = User(nome, idade, cargo, empresa, renda, meta, economiaMensal, poupanca)
+
+        userInfo(users, user)
+        return user
+    except ValueError:
+        print("Erro: Por favor, insira valores validos em campos númericos.")
+        return None 
+    
+#########################################################################
+
+# FUNÇÃO PARA ADICIONAR INFORMAÇÕES NO MySQL (C)
 def add(user: User):
     comando = f"""
     INSERT INTO meta (nome, idade, cargo, empresa, salario, meta, economiaMensal, poupanca, mesesRestantes)
@@ -51,7 +128,7 @@ def add(user: User):
                         user.idade,
                         user.cargo,
                         user.empresa,
-                        user.salario,
+                        user.renda,
                         user.meta,
                         user.economiaMensal,
                         user.poupanca,
@@ -66,18 +143,4 @@ def add(user: User):
     except mysql.connector.Error as erro:
         print(f"Erro: {erro}")
 
-
-#solicitando dados do usuario
-def userInfoRequest():
-    nome = input("Nome: \n")
-    idade = input("Idade: \n")
-    cargo = input("Cargo: \n")
-    empresa = input("Empresa:\n")
-    salario = input("Salário: R$\n")
-    meta = input("Quanto pretende juntar?\n")
-    economiaMensal = input("Quanto costuma guardar por mês?\n")
-    poupanca = input("Quanto você já possui? \n")
-    
-    user = User(nome, idade, cargo, empresa, salario, meta, economiaMensal, poupanca)
-    
-    return user;
+#########################################################################
